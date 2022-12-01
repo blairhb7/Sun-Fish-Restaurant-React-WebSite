e6418af4638fdb0a46ed557124527b97950fe678
blair chappell <blairhb7@yahoo.com>
2022-11-30 22:21:12 -0800

    7th try

--- components/StoreItem.tsx
+++ components/StoreItem.tsx
@@ -32,12 +32,12 @@ export function StoreItem({ id, name, Name, price, imgUrl, info,  }: StoreItemPr
         </div>
         
         <div className="flex ">
-        <span className='text-sm flex justify-end text-white px-3 w-8 lg:w-40 py-2'>{price}</span>
+        <span className='text-sm flex justify-end text-white px-3 w-8 lg:w-40 pt-3  py-2'>{price}</span>
             {quantity === 0 ? (
                 <button  className=' w-16 md:w-32 h-8 rounded-full text-xs md:text-sm my-2 text-white bg-red-500' onClick={() => increaseCartQuantity(id)}> + Add To Cart</button>
             ) : <div className='flex items-center flex-col gap-1'>
-                    <div className="flex items-center justify-center gap-1 pr-4">
-                        <button className="bg-yellow-500 rounded-md w-8 py-1" onClick={() => increaseCartQuantity(id)}>+</button>
+                    <div className="flex lg:flex-row flex-col items-center justify-center  gap-1 pr-4">
+                        <button className="bg-yellow-500 rounded-md  w-8 py-1" onClick={() => increaseCartQuantity(id)}>+</button>
                         <div className=" ">
                             <span className=" text-sm md:text-md text-white">{quantity} In Cart</span> 
                         </div>
